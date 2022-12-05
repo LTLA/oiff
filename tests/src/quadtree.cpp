d3fd40dd6aa35be17bb0259a582ee4eafbcb5653
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-05 22:53:11 +1100

    Test that the counter works correctly with ties.

--- tests/src/quadtree.cpp
+++ tests/src/quadtree.cpp
@@ -4,18 +4,34 @@
 #include <vector>
 #include <random>
 
-class QuadTreeCountTest : public ::testing::TestWithParam<std::tuple<int, int> > {};
+class QuadTreeCountTest : public ::testing::TestWithParam<std::tuple<int, int, bool> > {};
 
 TEST_P(QuadTreeCountTest, CountUnder) {
     auto param = GetParam();
     size_t nobs = std::get<0>(param);
     size_t seed = std::get<1>(param);
+    bool has_ties = std::get<2>(param);
 
     std::vector<int> prank;
     std::vector<int> crank;
-    for (size_t i = 0; i < nobs; ++i) {
-        prank.push_back(i);
-        crank.push_back(i);
+
+    if (has_ties) {
+        std::mt19937_64 rng(seed);
+        std::uniform_real_distribution dist;
+
+        int counter = 0;
+        for (size_t i = 0; i < nobs; ++i) {
+            if (dist(rng) <= 0.9) { // occasional ties.
+                ++counter;
+            }
+            prank.push_back(counter);
+            crank.push_back(counter);
+        }
+    } else {
+        for (size_t i = 0; i < nobs; ++i) {
+            prank.push_back(i);
+            crank.push_back(i);
+        }
     }
 
     std::mt19937_64 rng(seed);
@@ -43,7 +59,8 @@ INSTANTIATE_TEST_SUITE_P(
     QuadTreeCountTest,
     ::testing::Combine(
         ::testing::Values(100, 200, 500), // number of observations
-        ::testing::Values(0, 12345, 987654321) // seed
+        ::testing::Values(0, 12345, 987654321), // seed
+        ::testing::Values(false, true) // ties
     )
 );
 
