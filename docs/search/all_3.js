76fee95b9ca1dbdfabdf9096930e3067534f23d8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-07 23:43:46 +0000

    Deploying to gh-pages from @ LTLA/oiff@13882173b7701e77868f81d1a0d389fef939af46 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['random_5fseed_0',['random_seed',['../structoiff_1_1OptimizeFilter.html#a6e955f7788fe6b3ed4fe2c47afa3e95d',1,'oiff::OptimizeFilter']]],
-  ['run_1',['run',['../structoiff_1_1OptimizeFilter.html#ae4f78a279b7a3c22d0cf8f9dba44f9b3',1,'oiff::OptimizeFilter']]],
-  ['run_5fsubsample_2',['run_subsample',['../structoiff_1_1OptimizeFilter.html#aff766441d0234075a24d79c89ea80bab',1,'oiff::OptimizeFilter']]]
+  ['retain_5flarger_1',['retain_larger',['../structoiff_1_1OptimizeFilter.html#a6c4a446b10d0188c70437ec860fde160',1,'oiff::OptimizeFilter']]],
+  ['run_2',['run',['../structoiff_1_1OptimizeFilter.html#ae4f78a279b7a3c22d0cf8f9dba44f9b3',1,'oiff::OptimizeFilter']]],
+  ['run_5fsubsample_3',['run_subsample',['../structoiff_1_1OptimizeFilter.html#aff766441d0234075a24d79c89ea80bab',1,'oiff::OptimizeFilter']]]
 ];
