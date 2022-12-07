9b383713f33ad9494effa2ec52c6c37a45864856
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-07 16:17:02 +1100

    Fleshed out the unit tests.

--- tests/src/oiff.cpp
+++ tests/src/oiff.cpp
@@ -72,7 +72,7 @@ TEST_P(ScenarioTest, Uniform) {
     EXPECT_EQ(best, ref);
 }
 
-TEST_P(ScenarioTest, CorrelatedUniform) {
+TEST_P(ScenarioTest, PerfectCorrelation) {
     auto param = GetParam();
     size_t nobs = std::get<0>(param);
     int seed = std::get<1>(param);
@@ -90,6 +90,80 @@ TEST_P(ScenarioTest, CorrelatedUniform) {
     // Now the pvalues and covariates are correlated.
     std::sort(pvalues.begin(), pvalues.end());
     std::sort(covariates.begin(), covariates.end());
+
+    auto best = oiff::find_optimal_filter(nobs, pvalues.data(), covariates.data(), 0.05);
+    auto ref = reference(pvalues, covariates, 0.05);
+    EXPECT_EQ(best, ref);
+}
+
+TEST_P(ScenarioTest, ImperfectCorrelation) {
+    auto param = GetParam();
+    size_t nobs = std::get<0>(param);
+    int seed = std::get<1>(param);
+
+    std::mt19937_64 rng(seed);
+    std::normal_distribution ndist;
+
+    // Imperfect correlation between p-values and the covariates.
+    std::vector<double> pvalues, covariates;
+    for (size_t i = 0; i < nobs; ++i) {
+        pvalues.push_back(static_cast<double>(i + 1) / (nobs + 1));
+        covariates.push_back(ndist(rng) * 0.01 + static_cast<double>(i) / nobs);
+    }
+
+    auto best = oiff::find_optimal_filter(nobs, pvalues.data(), covariates.data(), 0.05);
+    auto ref = reference(pvalues, covariates, 0.05);
+    EXPECT_EQ(best, ref);
+}
+
+TEST_P(ScenarioTest, LowerCovariates) {
+    auto param = GetParam();
+    size_t nobs = std::get<0>(param);
+    int seed = std::get<1>(param);
+
+    std::mt19937_64 rng(seed);
+    std::uniform_real_distribution udist;
+    std::normal_distribution ndist;
+
+    std::vector<double> pvalues, covariates;
+    size_t most = nobs * 0.9;
+    for (size_t i = 0; i < most; ++i) {
+        pvalues.push_back(udist(rng));
+        covariates.push_back(ndist(rng));
+    }
+
+    // Smaller p-values associated with lower covariates.
+    for (size_t i = 0; i < nobs - most; ++i) {
+        pvalues.push_back(udist(rng)/50);
+        covariates.push_back(ndist(rng) - 1);
+    }
+
+    auto best = oiff::find_optimal_filter(nobs, pvalues.data(), covariates.data(), 0.05);
+    auto ref = reference(pvalues, covariates, 0.05);
+    EXPECT_EQ(best, ref);
+}
+
+TEST_P(ScenarioTest, HigherCovariates) {
+    auto param = GetParam();
+    size_t nobs = std::get<0>(param);
+    int seed = std::get<1>(param);
+
+    std::mt19937_64 rng(seed);
+    std::uniform_real_distribution udist;
+    std::normal_distribution ndist;
+
+    std::vector<double> pvalues, covariates;
+    size_t most = nobs * 0.5;
+    for (size_t i = 0; i < most; ++i) {
+        pvalues.push_back(udist(rng));
+        covariates.push_back(ndist(rng));
+    }
+
+    // Smaller p-values associated with higher covariates.
+    for (size_t i = 0; i < nobs - most; ++i) {
+        pvalues.push_back(udist(rng)/50);
+        covariates.push_back(ndist(rng) + 1);
+    }
 
     auto best = oiff::find_optimal_filter(nobs, pvalues.data(), covariates.data(), 0.05);
     auto ref = reference(pvalues, covariates, 0.05);
